4f0c0f740f022d7b8a00e69af04fef4887a0917f
pratikbabar97 <pratikbabar2199@gmail.com>
2024-07-30 23:16:04 +0530

    First commit

---
+++ practice.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize two variables
+        int number1 = 10;
+        int number2 = 20;
+        
+        // Add the two numbers
+        int sum = number1 + number2;
+        
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is " + sum);
+    }
+}
