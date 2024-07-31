35b0e9baa4048032445cb36071f090dd519905ed
pratikbabar97 <pratikbabar2199@gmail.com>
2024-07-31 08:56:33 +0530

    version 2.0

--- Subtraction.java
+++ Subtraction.java
@@ -6,6 +6,7 @@ public class Subtraction {
 
         // Perform subtraction
         int result = number1 - number2;
+        result = result - number2;
 
         // Print the result
         System.out.println("The result of subtracting " + number2 + " from " + number1 + " is: " + result);
