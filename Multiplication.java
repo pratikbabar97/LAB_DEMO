daf1a4d2a5657d791adde7b2d0d05f16b90a60cb
pratikbabar97 <pratikbabar2199@gmail.com>
2024-07-31 08:40:09 +0530

    multi commmit

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        // Define two numbers to multiply
+        int number1 = 5;
+        int number2 = 10;
+
+        // Perform multiplication
+        int result = number1 * number2;
+
+        // Print the result
+        System.out.println("The result of multiplying " + number1 + " and " + number2 + " is: " + result);
+    }
+}
