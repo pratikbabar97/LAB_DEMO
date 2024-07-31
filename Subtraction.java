a36a54890fb98519db5db5836402ad9bdd102c74
pratikbabar97 <pratikbabar2199@gmail.com>
2024-07-31 08:52:34 +0530

    sub code 1.0

---
+++ Subtraction.java
@@ -0,0 +1,13 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        // Define two numbers to subtract
+        int number1 = 15;
+        int number2 = 5;
+
+        // Perform subtraction
+        int result = number1 - number2;
+
+        // Print the result
+        System.out.println("The result of subtracting " + number2 + " from " + number1 + " is: " + result);
+    }
+}
